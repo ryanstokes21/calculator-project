a453529773ef81c5786cca2b4703f034591abf71
Ryan Stokes <ryanstokescfbre@gmail.com>
2026-07-17 16:45:59 -0500

    create operate function
    this will take the operator the user wants and
    its operands and call the function that does that
    operation

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
+let operand1;
+let operand2;
+let operator;
+
 function add(num1, num2) {
   return num1 + num2;
 }
-
-console.log(add(1, 3));
 
 function subtract(num1, num2) {
   return num1 - num2;
 }
 
-console.log(subtract(3, 1));
-
 function multiply(num1, num2) {
   return num1 * num2;
 }
-
-console.log(multiply(3, 2));
 
 function divide(num1, num2) {
   return num1 / num2;
 }
 
-console.log(divide(4, 2));
+function operate(operand1, operand2, operator) {
+  switch (operator) {
+    case '+':
+      add(operand1, operand2);
+      break;
+    case '-':
+      subtract(operand1, operand2);
+      break;
+    case 'x':
+      multiply(operand1, operand2);
+      break;
+    case '÷':
+      multiply(operand1, operand2);
+      break;
+    default:
+      return NaN;
+  }
+}
