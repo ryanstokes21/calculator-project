93ae1f35bad3db62b22869ad6221c48d5a932bf1
Ryan Stokes <ryanstokescfbre@gmail.com>
2026-07-16 20:17:47 -0500

    add basic math functions for operations

---
+++ script.js
@@ -0,0 +1,23 @@
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(add(1, 3));
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+console.log(subtract(3, 1));
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+console.log(multiply(3, 2));
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+console.log(divide(4, 2));
